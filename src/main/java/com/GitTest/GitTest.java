5d69f9a4005686ebafd3c74739b55589ea646abb
ZinniaHS <909623153@qq.com>
2021-06-29 22:55:12 +0800

    hot-fix commit1

--- src/main/java/com/GitTest/GitTest.java
+++ src/main/java/com/GitTest/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("third commit");
+        System.out.println("hot-fix commit");
     }
 }
